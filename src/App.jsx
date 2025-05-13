a2b44e2c6e923b50e4ddf18181792927bc481c75
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-05-13 06:54:27 +0600

    checked is the bootstrap is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1 className="container">home</h1>} />
+        <Route
+          path="/"
+          element={
+            <h1 className="container">
+              home
+              <Button variant="info" className="">
+                click me
+              </Button>
+            </h1>
+          }
+        />
         <Route path="/login" element={<h1>login</h1>} />
       </Routes>
     </BrowserRouter>
