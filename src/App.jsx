8c9deb058dc7c877d251f75796973986b9cbf1ba
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-05-13 06:43:03 +0600

    added routing and fixed bootstrap import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1 className="container">home</h1>} />
+        <Route path="/login" element={<h1>login</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
